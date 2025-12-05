c6658d4af5fc9309af2c07f4b748b099c77821fc
lequochoi1910 <lequochoi7390@gmail.com>
2025-12-05 22:17:12 +0700

    upload file

---
+++ index.js
@@ -0,0 +1,58 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document
+    .querySelectorAll("button")
+    [i].addEventListener("click", function handleClick() {
+      makeSound(this.innerHTML); //this.innerHTML: trả về chuỗi HTML nằm bên trong tag đó.
+      buttonAnimation(this.innerHTML);
+    });
+}
+
+//Detecting keyboard press
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(this.innerHTML);
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() { //setTimeout method calls a function after a number of milliseconds.
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
